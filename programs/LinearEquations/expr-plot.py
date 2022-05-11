a60d0151b23405cde9a9032e50d430272ae5039e
Sandeep Deb <deb.sandeep@gmail.com>
2022-05-11 21:33:40 +0530

    Updates to gitignore and added expr-plot.py

---
+++ programs/LinearEquations/expr-plot.py
@@ -0,0 +1,24 @@
+# Takes a linear equation and plots LHS and RHS as two
+# line graphs using matplotlib
+
+import matplotlib.pyplot as plt
+
+expr = "x+2 = 16-x"
+parts = expr.split( '=' )
+lhExpr = parts[0].strip()
+rhExpr = parts[1].strip()
+
+lhYVals = []
+rhYVals = []
+xVals   = []
+
+for x in range(-20,20):
+    xVals.append( str(x) )
+    lhYVals.append( eval( lhExpr ) )
+    rhYVals.append( eval( rhExpr ) )
+
+plt.plot( xVals, lhYVals )
+plt.plot( xVals, rhYVals )
+plt.grid()
+plt.legend( [lhExpr, rhExpr] )
+plt.show()
