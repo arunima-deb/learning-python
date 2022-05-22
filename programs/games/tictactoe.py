c2f2049bdbfefbf6631d5d8ac5848e23bef9fb92
Arunima Deb <arunima.s.deb@gmail.com>
2022-05-22 21:32:31 +0530

    tic tac toe changes

--- programs/games/tictactoe.py
+++ programs/games/tictactoe.py
@@ -36,19 +36,76 @@ def printBoard():
         rowNum += 1
         print( "\n", OFFSET, " +---+---+---+" )
 
-def getUserInput( turn ):
-    if turn%2 == 1:
+def getUserInput( player ):
+    if player == 1:
         prompt = "\nEnter where you want to place X : "
     else:
         prompt = "\nEnter where you want to place O : "
     move = input( prompt )
     return move
+
+def getBoardCoordinate( move ):
+    # move is a 2-character string
+    # first char = column (a, b, c)
+    # second char = row (1, 2, 3)
+    columnChar = move[0]
+    colNum = 999999
+    if columnChar == 'a':
+        colNum = 0
+    elif columnChar == 'b':
+        colNum = 1
+    elif columnChar == 'c':
+        colNum = 2
+        
+    rowChar = move[1]
+    rowNum = 999999
+    if rowChar == '1':
+        rowNum = 0
+    elif rowChar == '2':
+        rowNum = 1
+    elif rowChar == '3':
+        rowNum = 2
+
+    coordinates = [colNum, rowNum]    
+    return coordinates
+
+def updateBoard( coordinates, player ):
+    rowNum = coordinates[1]
+    colNum = coordinates[0]
+    if player == 1:
+        board[rowNum][colNum] = 1
+    else:
+        board[rowNum][colNum] = 2
+
+def checkWin( player ):
+# return values:
+# Game still on = 0
+# X wins = 1
+# O wins = 2
+# Draw = 3
+    if board[0][0] == player:
+        if board[0][1] == player:
+            if board[0][2] == player:
+                if player == 1:
+                    print( 'X wins!!' )
+                else:
+                    print( 'O wins!!' )
     
+def getCurrentPlayer( turn ):
+    if turn%2 == 1:
+        return 1
+    else:
+        return 2
+        
 def loopGame():
     turn = 1
     while True:                
         printBoard()
-        userInput = getUserInput( turn )
-        turn += 1   
+        player = getCurrentPlayer( turn )
+        userInput = getUserInput( player )
+        coordinates = getBoardCoordinate( userInput )
+        updateBoard( coordinates, player )
+        checkWin( player )
+        turn += 1
 
 loopGame()
