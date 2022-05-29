8caee752f1b69e2d794bacefb0def4929c22e9f4
Arunima Deb <arunima.s.deb@gmail.com>
2022-05-29 14:46:44 +0530

    Proper Fractions Simplifier v 1.0

--- programs/numbers/Proper Fractions Simplifier WIP.py
+++ programs/numbers/Proper Fractions Simplifier WIP.py
@@ -1,25 +1,55 @@
+# Collect Numerator
 promptNumerator = "Enter numerator : "
 numerator = int(input(promptNumerator))
 
+# Collect Denominator, return error if Denominator is zero
 promptDenominator = "Enter denominator : "
 denominator = int(input(promptDenominator))
-if '0' in str(denominator):
-    print( 'I', 'n', 'v', 'a', 'l', 'i', 'd', sep='' )
+if denominator == 0:
+    print( '\nCannot divide by zero!!', sep='' )
 
+# Prints  fraction without simplifying
 def printFraction( numerator, denominator ):
-    print()
-    print( '\t\t', numerator, sep='' )
-    print( 'Fraction:   ', '   ---', sep='' )
-    print( '\t\t', denominator, sep='' )
-    print()
+    if denominator == 0:
+        print( ' ' )
+    else:    
+        print()
+        print( '\t\t', numerator, sep='' )
+        print( 'Fraction:   ', '   ---', sep='' )
+        print( '\t\t', denominator, sep='' )
+        print()
 
-def factorize( denominator ):
-   print("The factors of", denominator, "are : ")
-   for i in range(1, denominator+1):
-       if denominator % i == 0:
-           print( (denominator/i) )
+# This function takes an integer value and returns an array of
+# integers, each of which are factors of the given number
+def factorize( value ):
+    factors = []
+    if value != 0:       
+        for number in range(1, value+1):
+            if value % number == 0:
+                factors.append(number)
+    return factors
 
-
+# Finds and returns highest common factor of numerator and denominator
+def findHCF( factors1, factors2 ):
+    commonFactors = []
+    for factor in factors1:
+        if factor in factors2:
+            commonFactors.append(factor)
+    return max(commonFactors)
 
 printFraction( numerator, denominator )
-factorize( denominator )
+numeratorFactors = factorize( numerator )
+denominatorFactors = factorize( denominator )
+
+def simplify( numerator, denominator, HCF ):
+    simNumerator = ( int(numerator/HCF) )
+    simDenominator = ( int(denominator/HCF) )
+    print( 'Simplified Fraction = ', simNumerator, '/', simDenominator, sep='' )
+
+print( "Factors of numerator", numerator, "are", numeratorFactors )
+print( "Factors of denominator", denominator, "are", denominatorFactors )
+
+HCF = findHCF( numeratorFactors, denominatorFactors )
+print( 'HCF = ', HCF )
+
+simplify( numerator, denominator, HCF )
--- programs/numbers/Simple Calculator WIP.py
+++ programs/numbers/Simple Calculator WIP.py
@@ -5,13 +5,32 @@ print( '\n   Simple Calculator v. 1.0\n' )
 prompt = 'Enter sum : '
 problem = str(input(prompt))
 
-def determineOperation( problem ):   
+def determineOperation( problem ):
+    operation = 'E'
+    
     if '+' in problem:
-        print(problem.split( '+' ))
-    if '-' in problem:
-        print(problem.split( '-' ))
-    if 'x' in problem:
-        print(problem.split( 'x' ))
-
+        addition = problem.split( '+' )
+        operation = '+'
+        print(addition)
         
+    if '-' in problem:
+        subtraction = problem.split( '-' )
+        operation = '-'
+        
+    if 'x' in problem:
+        multiplication = problem.split( 'x' )
+        operation = 'x'
+
+    if '/' in problem:
+        division = problem.split( '/' )
+        operation = '/'
+
+    return operation
+    return addition
+        
+def addition(addition, operation):
+    print( (addition(0)) + (addition(1)) )
+    
+
 determineOperation( problem )        
+addition(addition, operation)
