b4365318e14d833fb1309abc70d2cb617f63936b
Arunima Deb <arunima.s.deb@gmail.com>
2022-06-04 13:45:34 +0530

    hangman WIP

--- programs/testingfunctions.py
+++ programs/testingfunctions.py
@@ -1,7 +1,6 @@
-x = []
-y = 'abcdefg'
-for i in y:
-    x.append(i)
-
-for c in y:
-    print(c, end=" ")
+import random
+def word_selected(fname):
+    word_file = open('hangman.txt','r+')
+    secret_word = random.choice(word_file.read().split())
+    word_file.close()
+    return secret_word
