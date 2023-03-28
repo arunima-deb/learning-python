8fe709ce9b1cebd0aa635ad3bebf33461f0a6baf
Arunima Deb <arunima.s.deb@gmail.com>
2023-03-28 21:04:32 +0530

    Percentage Composition calculator WIP

---
+++ programs/Chemistry/Percentage Composition.py
@@ -0,0 +1,37 @@
+# Data/Variables
+global elements
+global atomicMasses
+global evenNum
+global allTermsinFile
+global atomicNum
+
+elements = []
+atomicMasses = []
+evenNum = 0
+allTermsinFile = []
+atomicNum = int(input("Enter atomic number : "))
+
+# Collects RAM of elements 1-30
+def getAtomicMass():
+    for i in range(1, 31):
+        if i == 1:
+            atomicMasses.append(int(1))
+        elif i == 4:
+            atomicMasses.append(int(9))
+        elif i == 7:
+            atomicMasses.append(int(14))
+        elif i == 18:
+            atomicMasses.append(int(40))
+        else:
+            if i%2 == 0:
+                atomicMasses.append(int(2*i))
+            else:
+                atomicMasses.append(int(2*i+1))
+
+# Prints Mass number of an element taking it's atomic number as input
+def printAtomicMass( atomicNum ):
+    print( "Atomic mass of the ", atomicNum, "th element = ", atomicMasses[atomicNum-1], sep = '' )
+    print( "\n" )
+    
+getAtomicMass()
+printAtomicMass( atomicNum )
